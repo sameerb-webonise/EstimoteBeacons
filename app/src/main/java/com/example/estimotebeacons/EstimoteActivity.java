0634e70ba9ace9190176b4da6c9ef366a8db5b37
sameerb-webonise <sameer.belsare@weboniselab.com>
2017-08-11 12:27:19 +0530

    Initial commit POC RegionListener

---
+++ app/src/main/java/com/example/estimotebeacons/EstimoteActivity.java
@@ -0,0 +1,146 @@
+package com.example.estimotebeacons;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
+import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.recognition.packets.Beacon;
+import com.estimote.coresdk.service.BeaconManager;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class EstimoteActivity extends AppCompatActivity {
+
+    private static final Map<String, List<String>> PLACES_BY_BEACONS;
+    private static final String TAG = EstimoteActivity.class.getName();
+    private String lastDiscoveredBeacon = "";
+    private TextView textView;
+
+    // TODO: replace "<major>:<minor>" strings to match your own beacons.
+    static {
+        Map<String, List<String>> placesByBeacons = new HashMap<>();
+        placesByBeacons.put("36015:56457", new ArrayList<String>() {{
+            add("ICE");
+            /*// read as: "Heavenly Sandwiches" is closest
+            // to the beacon with major 22504 and minor 48827
+            add("Green & Green Salads");
+            // "Green & Green Salads" is the next closest
+            add("Mini Panini");
+            // "Mini Panini" is the furthest away*/
+        }});
+        placesByBeacons.put("12830:49469", new ArrayList<String>() {{
+            add("BLUEBERRY");
+            /*add("Green & Green Salads");
+            add("Heavenly Sandwiches");*/
+        }});
+        placesByBeacons.put("45892:23678", new ArrayList<String>() {{
+            add("MINT");
+            /*add("Green & Green Salads");
+            add("Heavenly Sandwiches");*/
+        }});
+        PLACES_BY_BEACONS = Collections.unmodifiableMap(placesByBeacons);
+    }
+
+    private List<String> placesNearBeacon(Beacon beacon) {
+        String beaconKey = String.format("%d:%d", beacon.getMajor(), beacon.getMinor());
+        if (PLACES_BY_BEACONS.containsKey(beaconKey)) {
+            return PLACES_BY_BEACONS.get(beaconKey);
+        }
+        return Collections.emptyList();
+    }
+
+    private BeaconManager beaconManager;
+    private BeaconRegion region;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estimote);
+        textView = (TextView) findViewById(R.id.tvInfo);
+        beaconManager = new BeaconManager(this);
+        beaconManager.setRangingListener(new BeaconManager.BeaconRangingListener() {
+
+            @Override
+            public void onBeaconsDiscovered(BeaconRegion beaconRegion, List<Beacon> beacons) {
+                if (!beacons.isEmpty()) {
+                    Beacon nearestBeacon = beacons.get(0);
+                    List<String> places = placesNearBeacon(nearestBeacon);
+                    // TODO: update the UI here
+                    //Log.d(TAG, "Nearest beacon: " + places);
+                    if(lastDiscoveredBeacon.equals("") || !lastDiscoveredBeacon.equals(places.get(0))) {
+                        Log.d(TAG, "lastDiscoveredBeacon: "+lastDiscoveredBeacon+"\nDiscovered beacon: " + places);
+                        String currentTime = Calendar.getInstance().getTime().toString();
+
+                        if(!lastDiscoveredBeacon.equals("")) {
+                            String titleExit = "\nExited region: " + lastDiscoveredBeacon;
+                            textView.setText(textView.getText() + "\n" + titleExit + "\nAT " + currentTime);
+                        }
+                        lastDiscoveredBeacon = places.get(0);
+                        String titleEntry = "Entered region: "+ lastDiscoveredBeacon;
+                        String message = beaconRegion.getProximityUUID()+":"+beaconRegion.getMajor()+":"+beaconRegion.getMinor();
+                        showNotification(titleEntry,
+                                message);
+                        textView.setText(textView.getText()+"\n"+titleEntry+"\nAT "+ currentTime);
+                    }
+                }
+
+            }
+        });
+
+        region = new BeaconRegion("ranged region", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startRanging(region);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        beaconManager.stopRanging(region);
+
+        super.onPause();
+    }
+
+    public void showNotification(String title, String message) {
+        Context applicationContext = getApplicationContext();
+        //Toast.makeText(applicationContext, title+"\n"+message, Toast.LENGTH_SHORT).show();
+        Intent notifyIntent = new Intent(applicationContext, EstimoteActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(applicationContext, 0,
+                new Intent[]{notifyIntent}, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(applicationContext)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
+    }
+}
---
+++ app/src/main/java/com/example/estimotebeacons/MyApplication.java
@@ -0,0 +1,105 @@
+package com.example.estimotebeacons;
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.estimote.coresdk.common.config.EstimoteSDK;
+import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.recognition.packets.Beacon;
+import com.estimote.coresdk.service.BeaconManager;
+
+import java.util.List;
+import java.util.UUID;
+
+public class MyApplication extends Application {
+
+    private static final String TAG = MyApplication.class.getName();
+    private BeaconManager beaconManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        EstimoteSDK.initialize(getApplicationContext(), EstimoteSDK.getAppId() == null ? "App Id" : EstimoteSDK.getAppId(),
+                                    EstimoteSDK.getAppToken() == null ? "App Token" : EstimoteSDK.getAppToken());
+
+        beaconManager = new BeaconManager(getApplicationContext());
+        /*beaconManager.setMonitoringListener(new BeaconManager.BeaconMonitoringListener() {
+
+            @Override
+            public void onEnteredRegion(BeaconRegion beaconRegion, List<Beacon> beacons) {
+                if(beacons != null && beacons.size() > 0) {
+                    for (Beacon beacon : beacons) {
+                        String title = "Entered region: "+ beaconRegion.getIdentifier();
+                        String message = beacon.getProximityUUID()+":"+beacon.getMajor()+":"+beacon.getMinor();
+                        Toast.makeText(MyApplication.this, title+"\n"+message, Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, title+"\n"+message);
+                        showNotification(title,
+                                message);
+                    }
+                }
+            }
+
+
+            @Override
+            public void onExitedRegion(BeaconRegion beaconRegion) {
+                //cancelNotification();
+                String title = "Exited region: "+ beaconRegion.getIdentifier();
+                String message = beaconRegion.getProximityUUID()+":"+beaconRegion.getMajor()+":"+beaconRegion.getMinor();
+                Toast.makeText(MyApplication.this, title+"\n"+message, Toast.LENGTH_SHORT).show();
+                Log.d(TAG, title+"\n"+message);
+                showNotification(title,
+                        message);
+            }
+        });*/
+
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startMonitoring(new BeaconRegion("ice",
+                        UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), 45892, 23678));
+            }
+        });
+        /*beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startMonitoring(new BeaconRegion("blueberry",
+                        UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), 12830, 49469));
+            }
+        });*/
+    }
+
+
+    public void cancelNotification() {
+        Toast.makeText(this, "cancelNotification", Toast.LENGTH_SHORT).show();
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(1);
+    }
+
+    public void showNotification(String title, String message) {
+        //Toast.makeText(this, title+"\n"+message, Toast.LENGTH_SHORT).show();
+        Intent notifyIntent = new Intent(this, EstimoteActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0,
+                new Intent[]{notifyIntent}, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
+    }
+}
